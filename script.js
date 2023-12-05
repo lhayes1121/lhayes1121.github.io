e3a3103b933b435d171e19421862ee2095507cac
Latoya H <142842059+lhayes1121@users.noreply.github.com>
2023-12-04 21:26:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//hamburger menu function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	} else {
+		menu.style.display = "block";
+	}
+}
